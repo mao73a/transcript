192db0071c6a283333a24b122f7b6aa6e409e8dd
mao73a <mao73a@o2.pl>
2024-04-27 23:33:20 +0200

    odtwarzanie danych historycznych, regeneracja podsumowan

--- src/app.js
+++ src/app.js
@@ -5,15 +5,27 @@ const multer = require('multer');
 const OpenAI  = require('openai');
 const app = express();
 const { exec } = require('child_process');
-
+const config = require('./config');
 const favicon = require('serve-favicon');
+const { TextLoader } = require("langchain/document_loaders/fs/text");
+const { HumanMessage, SystemMessage } = require("langchain-core/messages");
+const { ChatOpenAI } = require("@langchain/openai");
+const markdown = require( "markdown" ).markdown;
+
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+const appRoute = `/${config.applicationName}`;
 const port = 3000;
 
 
 // Initialize the OpenAI object
 const openai = new OpenAI();
+
+
+function removeExtension(filePath){
+   str = filePath.replace(/ /g, "_")
+   return str.replace(/\.[^/.]+$/, "");
+}
 
 // Configure multer storage
 const storage = multer.diskStorage({
@@ -21,24 +33,46 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/');
  },
  filename: function(req, file, cb) {
-    cb(null, file.originalname);
+   const ext = path.extname(file.originalname);
+   const uniqueFileName = `${Date.now()}${ext}`;
+   cb(null, uniqueFileName); 
  }
 });
 
 const upload = multer({ storage: storage });
 
-// Mock processing function
-function processFile(filePath) {
- // Simulate processing time
- return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('Processing complete');
-    }, 2000);
- });
+function removeAudioFiles() {
+   // Specify the directory you want to clean
+   const directoryPath = path.join(__dirname, 'uploads');
+
+   // Read the directory
+   fs.readdir(directoryPath, (err, files) => {
+       if (err) {
+           console.error('Error reading directory:', err);
+           return;
+       }
+
+       // Filter out .ogg, .mp3, and .m4a files
+       const audioFiles = files.filter(file => {
+           const ext = path.extname(file).toLowerCase();
+           return ext === '.ogg' || ext === '.mp3' || ext === '.m4a'  || ext === '.mp4';
+       });
+
+       // Delete each file
+       audioFiles.forEach(file => {
+           const filePath = path.join(directoryPath, file);
+           fs.unlink(filePath, err => {
+               if (err) {
+                   console.error(`Error deleting file ${file}:`, err);
+               } else {
+                   console.log(`Deleted file ${file}`);
+               }
+           });
+       });
+   });
 }
 
-
-async function transcribeAudio(filePath, res) {
+async function transcribeAudio(filePath) {
    try {
       // Transcribe the file using OpenAI Whisper-1
       const transcription = await openai.audio.transcriptions.create({
@@ -46,24 +80,64 @@ async function transcribeAudio(filePath, res) {
          model: "whisper-1",
          language: "pl"
       });
-      outputFilePath = filePath + '.txt';
+      outputFilePath = removeExtension(filePath) + '.txt';
       fs.writeFileSync(outputFilePath, transcription.text);
-      res = {status:'OK', fulltext : transcription.text, filename : outputFilePath};
-      console.log('File transcription saved to ' + outputFilePath);
+      removeAudioFiles();
+      console.log('transcribeAudio: File transcription saved to ' + outputFilePath);      
+      return  {status:'OK', fulltext : markdown.toHTML("### Transkrypcja \n"+transcription.text), filename : outputFilePath};
+
    } catch (error) {
-      res = {status:'ERROR'};
-      console.error('Error transcribing file:', error);
+      console.error('transcribeAudio: Error transcribing file:', error);
+      return {status:'ERROR', fulltext:'', filename:''};
    }
 }  
 
+async function summarize(filePath, res){
+   try {
+      const loader = new TextLoader(filePath);
+      const [doc] = await loader.load();
+      const chat = new ChatOpenAI({
+         modelName: 'gpt-3.5-turbo',
+         temperature: 0.7,
+         max_tokens:5000
+     });
+      const { content } = await chat.invoke([
+         new SystemMessage(`Analizuję zapis spotkania działowego, aby utworzyć dokument, który będzie zawierał wszystkie istotne informacje poruszone podczas spotkania. Mówię tylko prawdę. Wymieniam wszystkie tematy omówione w czasie spotkania. Identyfikuję wszystkie wzmianki o wykonanych zadaniach, planach pracy i kwestiach mogących wpłynąć na projekt, a następnie uporządkuję te informacje w odpowiednich sekcjach: 
+         - ### Podsumowanie 
+         - ### Zadania wykonane 
+         - ### Zadania do wykonania 
+         - ### Tematy do analizy
+         - ### Problemy personalne
+         Jeśli w rozmowie padły wzmianki na tematy nieobecności lub niedostępności pracowników, umieszczę je w sekcji "Problemy personalne". Jeśli nie było takich wzmianek, to nie generuję sekcji ### Problemy personalne.
+         Jeśli stwierdzę że w czasie spotkania poruszono tematy niepasujące do powyższych sekcji, to wspomnę o nich w sekcji ### Pozostałe.
+         Ignoruję uwagi dotyczące nagrywania spotkania.
+         Wynik powienin być w formacie markdown.
+         Zaczynamy! `
+         ),
+         new HumanMessage(
+            doc.pageContent
+         ),
+     ]);
+     outputFilePath = removeExtension(filePath) + '.sum1.txt';
+     fs.writeFileSync(outputFilePath, content);
+     console.log('summarize: Summary saved to ' + outputFilePath);
+     return {status:'OK', text : content,  message: 'Summary complete.'};
+    
+   } catch (error) {
+      console.error('summarize: Error making summary:', error);      
+     return {status:'ERROR',  text : '', message: 'Summary error '+error};
+   }
+}
+
 // Route for uploading files
-app.post('/transcript/upload', upload.single('file'), async (req, res) => {
+app.post(appRoute+'/upload', upload.single('file'), async (req, res) => {
    try {
       console.log('upload1');
       // Assuming the uploaded file is saved in 'uploads/' directory
       const filePath = path.join(__dirname, 'uploads', req.file.filename);
-      const outputFile = filePath+'.ogg';
-      const command = 'ffmpeg -i '+filePath+' -vn -map_metadata -1 -ac 1 -c:a libopus -b:a 24k -application voip '+outputFile;
+      const taskid =  req.file.filename.match(/^\d+/)[0]; 
+      const outputFile = removeExtension(filePath) + '.ogg';
+      const command = 'ffmpeg -i "'+filePath+'" -vn -map_metadata -1 -ac 1 -c:a libopus -b:a 24k -application voip '+outputFile;
 
       // Execute the command
       exec(command, async (error, stdout, stderr) => {
@@ -72,7 +146,7 @@ app.post('/transcript/upload', upload.single('file'), async (req, res) => {
          }
          else { 
             console.log('upload OK');
-            await res.json({ status:"OK" , message: 'Uploading complete', filename : outputFile});         
+            await res.json({ status:"OK" , message: 'Uploading complete', filename : outputFile, taskid : taskid});         
          }
        });
    } catch (error) {
@@ -83,15 +157,16 @@ app.post('/transcript/upload', upload.single('file'), async (req, res) => {
 
 
 
-app.post('/transcript/transcribe', upload.none(),   async (req, res) => {
+
+app.post(appRoute+'/transcribe', upload.none(),   async (req, res) => {
    try {
        const filePath = req.body.filename;
-       if (!filePath) {
+      if (!filePath) {
            return res.status(400).json({ message: 'transcribe: File path is required.' });
        }
       const transcriptionResult = await transcribeAudio(filePath);
-        res.json({
-           status : transcriptionResult.fulltext,
+      res.json({
+           status : "OK",
            filename : transcriptionResult.filename,
            message: 'Transcription complete.',
            text: transcriptionResult.fulltext,
@@ -106,17 +181,73 @@ app.post('/transcript/transcribe', upload.none(),   async (req, res) => {
    }
 });
 
-app.post('/transcript/test', (req, res) => {
-   res.json({
-
-      message: 'Transcription complete.'
-  }); 
+app.post(appRoute+'/summary', upload.none(),   async (req, res) => {
+   try {
+       var filePath = req.body.filename;
+       const taskid = req.body.taskid;
+       if (taskid){
+         filePath = path.join(__dirname, 'uploads', taskid)+'.txt'
+       }
+       else if (!filePath) {
+           return res.status(400).json({ message: 'summary: File path is required.' });
+       }
+       const summary = await summarize(filePath);
+        res.json({
+           status : "OK",
+           message: 'Summary complete.',
+           text: markdown.toHTML(summary.text),
+       });
+   } catch (error) {
+       console.error('Error summarizing file:', error);
+       //res.status(500).json({ message: 'Error transcribing file', text:'' });
+       res.json({
+         status : 'ERROR',
+         message: 'Summarize error.'
+     });       
+   }
 });
 
-// Serve static files from the 'public' directory
-app.use('/transcript', express.static('public'));
 
-app.get('/transcript', (req, res) => {
+async function loadFile(filepath){
+      const loader = new TextLoader(filepath);
+      const [doc] = await loader.load();
+      doc.pageContent;
+      return doc.pageContent;
+};
+
+app.post(appRoute+'/restore',upload.none(),  async (req, res) => {
+   const taskid = req.body.taskid;
+   if (!taskid) {
+       return res.status(400).json({ message: 'restore: You must provide taskid.' });
+   }
+   try{  
+      const file1  = await  loadFile(path.join(__dirname, 'uploads', taskid)+'.txt');
+      const file2  = await  loadFile(path.join(__dirname, 'uploads', taskid)+'.sum1.txt');
+      var date = new Date(parseInt(taskid));
+
+      res.json({
+         status : "OK",
+         message: 'Odtworzone z historii z '+date.toLocaleString(),
+         fulltext: markdown.toHTML(file1),
+         summary: markdown.toHTML(file2)
+      });
+   } catch(error){
+      console.error('/restore', error);
+      res.json({
+         status : "ERROR",
+         message: 'Wystąpił problem. Prawdopodobnie plik został usunięty z serwera.',
+         fulltext: error,
+         summary: ''
+      });
+   }
+
+});
+
+
+// Serve static files from the 'public' directory
+app.use(appRoute+'/', express.static('public'));
+
+app.get(appRoute+'/', (req, res) => {
    // Send the 'index.html' file as a response
    res.sendFile('public', 'index.html');
 });
--- src/public/upload.js
+++ src/public/upload.js
@@ -1,10 +1,9 @@
+const appRoute = `/${config.applicationName}`;
+
 document.addEventListener('DOMContentLoaded', () => {
   const dropZone = document.getElementById('drop-zone');
   const memo1 = document.getElementById('memo1');
   const memo2 = document.getElementById('memo2');
-  const progressBarContainer = document.querySelector('.progress');
-  const progressBar = document.querySelector('.progress-bar');
-
 
   dropZone.addEventListener('dragover', (e) => {
       e.preventDefault();
@@ -18,8 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
   dropZone.addEventListener('drop', async (e) => {
       e.preventDefault();
       dropZone.classList.remove('dragover');
-
       const file = e.dataTransfer.files[0];
+      var taskid = 0;
       if (file.type !== 'audio/mp4' && file.type !== 'audio/mpeg' &&
           file.type !== 'audio/x-m4a' && file.type !== 'video/mp4'){
           alert('Please drop an M4A, MP4, MP3 file only. ('+file.type+')');
@@ -29,10 +28,10 @@ document.addEventListener('DOMContentLoaded', () => {
       const formData = new FormData();
       formData.append('file', file);
 
-      // Show animation while uploading
-      dropZone.textContent = 'Uploading...';
+
+      dropZone.textContent = 'Przesyłam plik...';
         {
-          fetch('/transcript/upload', {
+          fetch(appRoute+'/upload', {
               method: 'POST',
               body: formData
           })
@@ -41,11 +40,11 @@ document.addEventListener('DOMContentLoaded', () => {
           })
           .then((data) => {
             dropZone.textContent = data.message;
-
+            taskid = data.taskid;  
             const formData = new FormData();
             formData.append('filename', data.filename);
-            dropZone.textContent = 'Saving speech to text...';
-            return fetch('/transcript/transcribe', {
+            dropZone.textContent = 'Przetwarzam mowę na tekst...';
+            return fetch(appRoute+'/transcribe', {
                 method: 'POST',
                 body: formData
               });      
@@ -54,53 +53,102 @@ document.addEventListener('DOMContentLoaded', () => {
             return  response.json();
           })
           .then((data) => {
-            dropZone.textContent = 'Reading the shit...';
-          })          
+            const formData = new FormData();
+            formData.append('filename', data.filename);            
+            dropZone.textContent = 'Generowanie podsumowania...';
+            memo1.style.display = 'block';
+            memo2.style.display = 'block';              
+            memo1Text.innerHTML = data.text;            
+            memo2Text.textContent = "Analiza tekstu..."   
+            return fetch(appRoute+'/summary', {
+              method: 'POST',
+              body: formData
+            });                
+          })    
+          .then((response) => {
+            return  response.json();
+          })   
+          .then((data) => {
+            dropZone.textContent = data.message;  
+            SetTaskId(taskid);
+            dropZone.innerHTML = 'Gotowe. <a href="' + appRoute + '/index.html?taskid=' + taskid + '">Link do tego podsumowania.</a>';
+            memo1.style.display = 'block';
+            memo2.style.display = 'block';      
+            memo2Text.innerHTML = data.text;            
+          })                          
           .catch(function(error) {
             console.log('Error 2');
             dropZone.textContent = error;
             console.log(error);
           });
-   
-   
-          return;
-          let data;
-          console.log(response);           
-          if (response.ok) {
-                 
-              data = await response.json();
 
-              dropZone.textContent = data.message;    
-              formData.filename = data.filename;            
-              const response = await fetch('/transcript/transcribe', {
-                method: 'POST',
-                body: formData
-              });  
-   
-              if (response.ok) {
-
-                data = await response.json();
-                dropZone.textContent = data.message;    
-                formData.filename = data.filename;    
-
-                memo1.style.display = 'block';
-                memo2.style.display = 'block';      
-                memo1.textContent = data.text;
-                memo2.textContent = 'todo';   
-              } else {
-                const text = await response.text();
-                dropZone.textContent = `Error: ${text}`;               
-              }    
-          } else {
-              const text = await response.text();
-              dropZone.textContent = `Error: ${text}`;
-              console.error(`Error: ${text}`);
-          }
-
-          memo1.style.display = 'block';
       }
 
      // dropZone.textContent = 'Drop M4A or MP3 file here';
   });
 });
 
+
+function restore(taskid){
+  const dropZone = document.getElementById('drop-zone');
+  const memo1 = document.getElementById('memo1');
+  const memo2 = document.getElementById('memo2');
+  const formData = new FormData();
+
+  dropZone.textContent = "Ładuje dane historyczne..."
+  formData.append('taskid', taskid);
+
+  fetch(appRoute+'/restore', {
+    method: 'POST',
+    body: formData
+  })
+  .then((response) => {
+    return  response.json();
+  })   
+  .then((data) => {
+    dropZone.textContent = data.message;  
+    memo1.style.display = 'block';
+    memo2.style.display = 'block';              
+    memo1Text.innerHTML = data.fulltext;            
+    memo2Text.innerHTML = data.summary;  
+  })
+  .catch(function(error) {
+    dropZone.textContent = error;
+    console.log(error);
+  });  
+
+};
+
+function SetTaskId(taskid){
+  document.getElementById("_taskid").textContent = taskid;  
+}
+
+function GetTaskId(taskid){
+  return document.getElementById("_taskid").textContent;
+}
+
+function regenerate(ptaskid){
+  const dropZone = document.getElementById('drop-zone');  
+  const memo2 = document.getElementById('memo2');
+  dropZone.textContent = "Ponowne generowanie podsumowania..."
+  const formData = new FormData();
+  formData.append('taskid', ptaskid); 
+
+  fetch(appRoute+'/summary', {
+      method: 'POST',
+      body: formData
+    })
+  .then((response) => {
+    return  response.json();
+  })   
+  .then((data) => {
+    dropZone.textContent = data.message;  
+    dropZone.innerHTML = 'Gotowe. <a href="' + appRoute + '/index.html?taskid=' + taskid + '">Link do tego podsumowania.</a>';
+    memo2Text.innerHTML = data.text;            
+  })                          
+  .catch(function(error) {
+    console.log('Error 4');
+    dropZone.textContent = error;
+    console.log(error);
+  });
+}
